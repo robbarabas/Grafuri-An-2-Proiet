37cf7dd6b6904023c7ece21ef8fca0a66d76046b
robbarabas <robertbarabas42@gmail.com>
2025-04-05 15:53:53 +0300

    Add project files.

---
+++ BR_draw.h
@@ -0,0 +1,87 @@
+
+#include <iostream>
+using namespace std;
+class draw_matrix
+{
+public:
+    int Len, Wth;
+    char** Mat;
+
+    // Default constructor
+    draw_matrix() : Len(0), Wth(0), Mat(nullptr) {}
+
+    // Constructor for square matrix (Len x Len)
+    draw_matrix(int _Len) : Len(_Len), Wth(_Len)
+    {
+        Mat = new char* [Len];
+        for (int i = 0; i < Len; i++) {
+            Mat[i] = new char[Wth];
+            for (int j = 0; j < Wth; j++) {
+                Mat[i][j] = ' ';
+            }
+        }
+    }
+
+    // Constructor for rectangular matrix (Len x Wth)
+    draw_matrix(int _Len, int _Wth) : Len(_Len), Wth(_Wth)
+    {
+        Mat = new char* [Len];
+        for (int i = 0; i < Len; i++) {
+            Mat[i] = new char[Wth];
+            for (int j = 0; j < Wth; j++) {
+                Mat[i][j] = ' ';
+            }
+        }
+    }
+
+    // Destructor to free allocated memory
+    ~draw_matrix()
+    {
+        if (Mat) {
+            for (int i = 0; i < Len; i++)
+                delete[] Mat[i];
+            delete[] Mat;
+        }
+    }
+
+    // Print the matrix
+    void print()
+    {
+        for (int i = 0; i < Len; i++) {
+            for (int j = 0; j < Wth; j++) {
+                cout << Mat[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+  
+    void draw_point(int rad, int x, int y, char sym)
+    {
+        for (int i = max(0, x - rad); i <= min(Len - 1, x + rad); i++) {
+            for (int j = max(0, y - rad); j <= min(Wth - 1, y + rad); j++) {
+                Mat[i][j] = sym;
+            }
+        }
+    }
+    void draw_line(int x1, int y1, int x2, int y2, int rad, char sym)
+    {
+        int dx = abs(x2 - x1), dy = abs(y2 - y1);
+        int sx = (x1 < x2) ? 1 : -1;
+        int sy = (y1 < y2) ? 1 : -1;
+        int err = dx - dy;
+
+        while (true)
+        {
+            draw_point(rad, x1, y1, sym); 
+
+            if (x1 == x2 && y1 == y2) break;
+
+            int e2 = 2 * err;
+            if (e2 > -dy) { err -= dy; x1 += sx; } 
+            if (e2 < dx) { err += dx; y1 += sy; } 
+        }
+    }
+
+	
+};
---
+++ Source.cpp
@@ -0,0 +1,12 @@
+﻿#include "BR_draw.h"
+
+int main() {
+   
+    draw_matrix A(100,200);
+    A.draw_line(3, 3, 50, 50, 1, 'e');
+    A.draw_line(30, 10, 50, 50, 1, 'f');
+    A.draw_line(50, 50, 100, 100, 2, '/');
+    A.draw_line(50, 0, 50, 200, 1, 'X');
+    A.print();
+    return 0;
+}
